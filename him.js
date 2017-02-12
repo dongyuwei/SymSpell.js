b0d05599e8bd325bda6f494a26efd8b824057135
Dong Yuwei <newdongyuwei@gmail.com>
2017-02-12 18:51:18 +0800

    use websocket to get words

--- him.js
+++ him.js
@@ -2,17 +2,15 @@ var express = require('express');
 var Triejs = require('triejs');
 var googleDict = require('./dict/google_227800_words.json');
 
+var trie = null;
 function buildTrie (){
-    var trie = new Triejs();
+    trie = new Triejs();
     for (let word in googleDict) {
         trie.add(word);
     }
-    var result = trie.find('test').sort(function(w1, w2){
-        return googleDict[w2] - googleDict[w1];
-    });
-    console.log(result);
 }
 
+buildTrie();
 
 process.on('uncaughtException', function(err) {
     console.error('Caught exception: ', err);
@@ -32,3 +30,23 @@ app.listen(3000);
 console.log('################################################');
 console.log('server (pid: ' + process.pid + ') started! please visit http://127.0.0.1:3000');
 console.log('################################################');
+
+
+var WebSocketServer = require('uws').Server;
+var wss = new WebSocketServer({ port: 3001 });
+
+function onMessage(message, ws) {
+    console.log('received: ' + message);
+
+    var result = trie.find(message).sort(function(w1, w2){
+        return googleDict[w2] - googleDict[w1];
+    });
+    console.log(result);
+    ws.send(JSON.stringify(result));
+}
+
+wss.on('connection', function(ws) {
+    ws.on('message', function(message){
+        onMessage(message, ws);
+    });
+});
